dfdf97437c2fec7cb73cb3c0711bf82ecbc1b99a
Breno Salvador de Freitas <brenosalvadordefreitas@gmail.com>
2021-05-29 20:54:59 -0300

    First commit

---
+++ src/Home.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Button } from 'antd';
+
+import './Home.css';
+
+const Home: FC = () => (
+  <div className="Home">
+    <Button type="primary">Button</Button>
+  </div>
+)
+
+export default Home;
